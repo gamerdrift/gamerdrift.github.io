c6d5fb727bc17a43bed96ce8212afcfb77d669c9
GamerDrift <email.jafer@gmail.com>
2026-05-31 08:47:33 +0530

    Trigger deployment after export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enable static export
+  /* other config options */
 };
 
 export default nextConfig;
